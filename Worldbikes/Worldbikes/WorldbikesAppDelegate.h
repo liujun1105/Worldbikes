6aa828137c9c9cd0d4fa115486ad9a73401beb05
Jun Liu <lj831015@gmail.com>
2012-04-03 23:54:15 +0100

    Initial Commit

---
+++ Worldbikes/Worldbikes/WorldbikesAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WorldbikesAppDelegate.h
+//  Worldbikes
+//
+//  Created by a亲爱的 我自己 on 03/04/2012.
+//  Copyright (c) 2012 Ericsson Software Campus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WorldbikesAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
