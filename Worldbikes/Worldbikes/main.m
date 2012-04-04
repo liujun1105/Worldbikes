89fb5400e90fdf201b034c56560e726299502c5a
Jun Liu <lj831015@gmail.com>
2012-04-04 23:35:30 +0100

    UI Commit
    This is the initial commit for the UI parts of the Worldbikes App
    project.

---
+++ Worldbikes/Worldbikes/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Worldbikes
+//
+//  Created by a亲爱的 我自己 on 03/04/2012.
+//  Copyright (c) 2012 Ericsson Software Campus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WorldbikesAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WorldbikesAppDelegate class]));
+    }
+}
